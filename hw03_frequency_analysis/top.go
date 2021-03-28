532e4fbc2d8202c7c7341e5e324eaff711638e28
Sergey K <sereg.project@gmail.com>
2021-03-28 11:59:55 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,59 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var e = regexp.MustCompile(`[^!"#$%&'()*+,./:;<=>?@[\]^_{|}~]+`)
+
+func Top10(str string) []string {
+	fields := strings.Fields(str)
+	wordsFrequency := map[string]int{}
+
+	for _, v := range fields {
+		subs := e.FindAllStringSubmatch(v, -1)
+
+		for _, v := range subs {
+			trimmedStr := strings.Trim(v[0], "-")
+
+			if len(trimmedStr) > 0 {
+				wordsFrequency[strings.ToLower(trimmedStr)]++
+			}
+		}
+	}
+
+	usesFrequency := map[int][]string{}
+
+	for k, v := range wordsFrequency {
+		usesFrequency[v] = append(usesFrequency[v], k)
+	}
+
+	usesKeys := make([]int, 0, len(usesFrequency))
+	for k := range usesFrequency {
+		usesKeys = append(usesKeys, k)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(usesKeys)))
+
+	topItems := make([]string, 0, 10)
+	var upperBound int
+	for _, v := range usesKeys {
+		cntItems := len(topItems)
+
+		if cntItems == 10 {
+			break
+		}
+
+		if len(usesFrequency[v]) > (10 - cntItems) {
+			upperBound = 10 - cntItems
+		} else {
+			upperBound = len(usesFrequency[v])
+		}
+
+		sort.Strings(usesFrequency[v])
+		topItems = append(topItems, usesFrequency[v][:upperBound]...)
+	}
+
+	return topItems
 }
