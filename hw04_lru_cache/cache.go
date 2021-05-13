f0a89671d7014a4e7e94f3c98b219dc36e4f0865
Sergey K <sereg.project@gmail.com>
2021-05-13 10:24:03 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,11 +11,60 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	l.Lock()
+	defer l.Unlock()
+
+	item, ok := l.items[key]
+
+	if ok {
+		l.queue.MoveToFront(item)
+		item.Value = cacheItem{value: value, key: string(key)}
+
+		return true
+	}
+
+	if l.capacity == l.queue.Len() {
+		lastItem := l.queue.Back()
+
+		delete(l.items, Key(lastItem.Value.(cacheItem).key))
+		l.queue.Remove(lastItem)
+	}
+
+	newItem := cacheItem{value: value, key: string(key)}
+	addedItem := l.queue.PushFront(newItem)
+	l.items[key] = addedItem
+
+	return false
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	l.Lock()
+	defer l.Unlock()
+
+	item, ok := l.items[key]
+
+	if ok {
+		l.queue.MoveToFront(item)
+
+		return item.Value.(cacheItem).value, ok
+	}
+
+	return nil, false
+}
+
+func (l *lruCache) Clear() {
+	l.Lock()
+	defer l.Unlock()
+
+	l.queue = NewList()
+	l.items = make(map[Key]*ListItem, l.capacity)
 }
 
 type cacheItem struct {
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestList(t *testing.T) {
+func TestListBase(t *testing.T) {
 	t.Run("empty list", func(t *testing.T) {
 		l := NewList()
 
@@ -15,6 +15,144 @@ func TestList(t *testing.T) {
 		require.Nil(t, l.Back())
 	})
 
+	t.Run("PushFront", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+
+		l.PushFront(30) // [30, 10]
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, 30, l.Front().Value)
+		require.Equal(t, 10, l.Front().Next.Value)
+		require.Equal(t, 10, l.Back().Value)
+		require.Equal(t, 30, l.Back().Prev.Value)
+
+		l.PushFront(50) // [50, 30, 10]
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, 50, l.Front().Value)
+		require.Equal(t, 30, l.Front().Next.Value)
+		require.Equal(t, 50, l.Front().Next.Prev.Value)
+		require.Equal(t, 10, l.Front().Next.Next.Value)
+		require.Equal(t, 10, l.Back().Value)
+		require.Equal(t, 30, l.Back().Prev.Value)
+	})
+
+	t.Run("PushBack", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10) // [10]
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 10, l.Back().Value)
+		require.Equal(t, 10, l.Front().Value)
+
+		l.PushBack(30) // [10, 30]
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, 30, l.Back().Value)
+		require.Equal(t, 10, l.Back().Prev.Value)
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 30, l.Front().Next.Value)
+
+		l.PushBack(50) // [10, 30, 50]
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, 50, l.Back().Value)
+		require.Equal(t, 30, l.Back().Prev.Value)
+		require.Equal(t, 50, l.Back().Prev.Next.Value)
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 30, l.Front().Next.Value)
+		require.Equal(t, 10, l.Back().Prev.Prev.Value)
+	})
+
+	t.Run("Remove", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		l.Remove(l.Front())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+		require.Equal(t, 0, l.Len())
+
+		l.PushBack(10) // [10]
+		l.Remove(l.Back())
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
+		require.Equal(t, 0, l.Len())
+
+		l.PushFront(10)
+		l.PushBack("c") // [10, "c"]
+		l.Remove(l.Front())
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, "c", l.Front().Value)
+		require.Equal(t, "c", l.Back().Value)
+		require.Nil(t, l.Back().Prev)
+		require.Nil(t, l.Front().Next)
+
+		l.PushFront(10) // [10, "c"]
+		l.Remove(l.Back())
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 10, l.Back().Value)
+		require.Equal(t, 10, l.Front().Value)
+		require.Nil(t, l.Front().Next)
+		require.Nil(t, l.Back().Prev)
+
+		l.PushBack("c")
+		l.PushFront("a") // ["a", 10, "c"]
+
+		l.Remove(l.Front().Next) // ["a", "c"]
+		require.Equal(t, "a", l.Front().Value)
+		require.Equal(t, "c", l.Back().Value)
+		require.Equal(t, "c", l.Front().Next.Value)
+		require.Equal(t, "a", l.Back().Prev.Value)
+		require.Equal(t, 2, l.Len())
+	})
+
+	t.Run("MoveToFront", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		l.MoveToFront(l.Front())
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+
+		l.PushBack(20) // [10, 20]
+
+		l.MoveToFront(l.Back()) // [20, 10]
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, 20, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+		require.Equal(t, 10, l.Front().Next.Value)
+		require.Equal(t, 20, l.Back().Prev.Value)
+
+		l.PushFront("abc") // ["abc", 20, 10]
+		l.PushFront("new") // ["new", "abc", 20, 10]
+
+		l.MoveToFront(l.Front().Next) // ["abc", "new", 20, 10]
+		require.Equal(t, 4, l.Len())
+		require.Equal(t, "abc", l.Front().Value)
+		require.Equal(t, "new", l.Front().Next.Value)
+		require.Nil(t, l.Front().Prev)
+		require.Equal(t, "abc", l.Front().Next.Prev.Value)
+		require.Equal(t, 20, l.Front().Next.Next.Value)
+		require.Equal(t, 10, l.Back().Value)
+		require.Equal(t, 20, l.Back().Prev.Value)
+
+		l.MoveToFront(l.Front().Next) // ["new", "abc", 20, 10]
+
+		require.Equal(t, 4, l.Len())
+		require.Equal(t, "new", l.Front().Value)
+		require.Equal(t, "abc", l.Front().Next.Value)
+		require.Nil(t, l.Front().Prev)
+		require.Equal(t, "new", l.Front().Next.Prev.Value)
+		require.Equal(t, 20, l.Front().Next.Next.Value)
+		require.Equal(t, 10, l.Back().Value)
+		require.Equal(t, 20, l.Back().Prev.Value)
+	})
+}
+
+func TestListComplex(t *testing.T) {
 	t.Run("complex", func(t *testing.T) {
 		l := NewList()
 
